9840d0e7942e8c9eb6acec41251d569ba2d38d90
xxm <gold_like@hotmail.com>
2019-02-23 19:30:17 +0800

    添加Test工程至Git

---
+++ src/hehe/Test.java
@@ -0,0 +1,10 @@
+package hehe;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("heheheheh");
+        System.out.println("heheheheh");
+        System.out.println("heheheheh");
+    }
+
+}
